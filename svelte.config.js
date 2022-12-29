ecf00ea61ed7d843b7fe7d263af0b246c2ef354d
freitzzz <1160907@isep.ipp.pt>
2022-12-29 14:04:52 +0000

    define alias for components barrel file

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +9,10 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		alias: {
+			'@components': path.resolve('./src/lib/components/index.ts')
+		}
 	}
 };
 
