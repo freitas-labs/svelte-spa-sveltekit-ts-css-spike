8bdf687cc18778df859eb3d99b0386a75bc8d537
freitzzz <1160907@isep.ipp.pt>
2022-12-29 13:20:07 +0000

    test scope styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+	h1 {
+		font-family: monospace;
+	}
+	p {
+		font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu,
+			Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+	}
+</style>
