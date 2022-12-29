b230e50225cbc11fd7f2e5828320fc8f7b85f638
freitzzz <1160907@isep.ipp.pt>
2022-12-29 14:05:10 +0000

    use date component in page component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,11 @@
+<script>
+	import { Date } from '@components';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Date />
 
 <style>
 	h1 {
